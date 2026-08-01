78ace8c6c8319f8c65ef3e0dfc240b70dbfdb393
Abdurahmon <abdurrohmon654@gmail.com>
2026-08-01 17:51:18 +0500

    fix: media fayllarni production da ham serve qilish

--- scr/urls.py
+++ scr/urls.py
@@ -1,15 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path,include
-
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('panelim/', admin.site.urls),
-    path('',include('app.urls'))
-]
+    path('', include('app.urls')),
 
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Media fayllarni production da ham serve qilish (Railway uchun)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
